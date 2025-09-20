9ac97695a877487d87d30a697b09c6a8c2feff29
Santharuban <santharuban2803@gmail.com>
2025-09-20 20:32:53 +0530

    folder structure changes

--- source/script.js
+++
@@ -1,7 +0,0 @@
-function showContact() {
-  document.getElementById("contact").classList.remove("hidden");
-  window.scrollTo({
-    top: document.getElementById("contact").offsetTop,
-    behavior: "smooth"
-  });
-}
